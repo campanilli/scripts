50688ca259fb023e50a2f13ba30c66ba8510d2f9
campanilli <thiago.campanilli@gmail.com>
2020-07-15 03:22:24 +0000

    "Alterações realizadas em 15/07/2020-03:22"

---
+++ PRT_MDW_GIT_0.0.1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Autor: Thiago Campanilli
+ 
+cd /home/pi/CAMPANILLI/scripts
+
+Echo '"Alterações realizadas em '`date +%d/%m/%Y-%H:%M`'"'
+ 
+git status
+ 
+git add .
+ 
+git pull
+ 
+git status
+ 
+git commit -m '"Alterações realizadas em '`date +%d/%m/%Y-%H:%M`'"'
+ 
+git push -u origin master
